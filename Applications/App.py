d77da596b434928a73a72d2a3eec531fb4262406
sehwanMoon <37695581+SehwanMoon@users.noreply.github.com>
2025-07-23 18:25:04 +0900

    Update App.py

--- Applications/App.py
+++ Applications/App.py
@@ -133,7 +133,7 @@ if st.button("Predict Depression"):
 st.markdown("---")
 st.markdown(
     """
-    <p style="font-size:12px;">
+    <p style="font-size:13px;">
       Developed by 
       <span style="color:#005CA9; font-weight:bold;">
         Electronics and Telecommunications Research Institute (ETRI)
@@ -146,7 +146,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-f1, f2, f3  = st.columns([1,1.1,4])
+f0, f1, f2, f3  = st.columns([2,1,1.1,2])
 f1.image( os.path.join(BASE_DIR, "etri2.png"),    width=2056)
 f2.image(os.path.join(BASE_DIR, "chonnam.png"), width=2056)
 
