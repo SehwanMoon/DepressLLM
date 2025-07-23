c83e36ef70e14d1d1e2a01d067522e763df0a4e2
sehwanMoon <37695581+SehwanMoon@users.noreply.github.com>
2025-07-23 18:19:21 +0900

    Update App.py

--- Applications/App.py
+++ Applications/App.py
@@ -131,10 +131,11 @@ if st.button("Predict Depression"):
 # Footer: 기관 로고 & 문구
 # ------------------------------------------
 st.markdown("---")
-f1, f2, f3  = st.columns([1,1.2,3])
-f1.image( os.path.join(BASE_DIR, "etri2.png"),    width=2056)
-f2.image(os.path.join(BASE_DIR, "chonnam.png"), width=2056)
 st.markdown(
     "**Developed by  Electronics and Telecommunications Research Institute (ETRI) & Chonnam National University &**"
 )
+
+f1, f2, f3  = st.columns([1,1.1,4])
+f1.image( os.path.join(BASE_DIR, "etri2.png"),    width=2056)
+f2.image(os.path.join(BASE_DIR, "chonnam.png"), width=2056)
 
